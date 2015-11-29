4b96df773e16b9c1d820ec615ab7e7bf959c696b
Martin Hujer <mhujer@gmail.com>
2015-11-29 10:32:43 +0100

    Arguments and Options

--- src/Command/HelloCommand.php
+++ src/Command/HelloCommand.php
@@ -5,6 +5,7 @@ namespace App\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class HelloCommand extends Command
@@ -13,11 +14,22 @@ class HelloCommand extends Command
     {
         $this
             ->setName('zdrojak:hello')
-            ->setDescription('Jednoduchy Hello World!');
+            ->setDescription('Jednoduchy Hello World!')
+            ->addArgument('name', null, 'Koho zdravíme?', 'World')
+            ->addOption('backwards', 'b', null, 'Pozpátku?')
+            ->addOption('greeting', null, InputOption::VALUE_REQUIRED, 'Pozdrav', 'Hello');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('Hello World from Symfony Console!');
+        $message = sprintf(
+            '%s %s from Symfony Console!',
+            $input->getOption('greeting'),
+            $input->getArgument('name')
+        );
+        if ($input->getOption('backwards')) {
+            $message = strrev($message);
+        }
+        $output->writeln($message);
     }
 }
